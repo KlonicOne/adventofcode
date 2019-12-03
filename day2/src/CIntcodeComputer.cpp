9ceb6a47d0fa60d8bf703e3d2a3bf6b1f7acbebe
klonicone <KlonicOne@gmx.net>
2019-12-03 22:21:03 +0100

    Completed day2

--- day2/src/CIntcodeComputer.cpp
+++ day2/src/CIntcodeComputer.cpp
@@ -41,12 +41,30 @@ std::vector<int> CIntcodeComputer::getVectorCode(istream& input)
 
 std::vector<int> CIntcodeComputer::progressVectorCode(std::vector<int> vectorIntcode)
 {
+  int firstVal;
+  int secondVal;
+
   // Walk through given intcode in stepsize 4 until end reached
   for (std::vector<int>::const_iterator Opcode = vectorIntcode.begin(); *Opcode != OPCODE_END; Opcode += 4)
   {
-    std::cout << *Opcode << ' ';
+    //std::cout << *Opcode << ' ';
+    switch (*Opcode)
+    {
+    case (OPCODE_ADD):
+      firstVal = vectorIntcode.at(*Opcode + 1);
+      secondVal = vectorIntcode.at(*Opcode + 2);
+      vectorIntcode.at(*Opcode + 3) = firstVal + secondVal;
+      break;
+    case (OPCODE_MUL):
+      firstVal = vectorIntcode.at(*Opcode + 1);
+      secondVal = vectorIntcode.at(*Opcode + 2);
+      vectorIntcode.at(*Opcode + 3) = firstVal * secondVal;
+      break;
+    default:
+      break;
+    }
   }
-
+  this->debugOutVector(vectorIntcode);
   return (vectorIntcode);
 }
 
@@ -54,6 +72,6 @@ void CIntcodeComputer::debugOutVector(vector<int> inVector)
 {
   for (std::vector<int>::const_iterator i = inVector.begin(); i != inVector.end(); ++i)
   {
-    std::cout << *i << ' ';
+    std::cout << *i << ',' << ' ';
   }
 }
