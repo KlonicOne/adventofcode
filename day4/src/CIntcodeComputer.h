e5ded1f91c3dcd233b335c8547541f071cbb40bb
KlonicOne <KlonicOne@gmx.de>
2019-12-07 18:29:17 +0100

    Prepared day4 with two new commands in interpreter

---
+++ day4/src/CIntcodeComputer.h
@@ -0,0 +1,37 @@
+/*
+ * CIntcodeComputer.h
+ *
+ *  Created on: 3 Dec 2019
+ *      Author: nico
+ */
+
+#ifndef CINTCODECOMPUTER_H_
+#define CINTCODECOMPUTER_H_
+
+#include <vector>
+#include <istream>
+using namespace std;
+
+// Opcode defines
+#define OPCODE_END 99
+#define OPCODE_ADD 1
+#define OPCODE_MUL 2
+#define OPCODE_IN 3
+#define OPCODE_OUT 4
+
+class CIntcodeComputer
+{
+public:
+  CIntcodeComputer();
+  virtual ~CIntcodeComputer();
+
+  std::vector<int> getVectorCode(istream &input);
+  std::vector<int> progressVectorCode(std::vector<int> vectorIntcode);
+  int nounVerbResultProducedInput(std::vector<int> vectorIntcode, int targetVal);
+
+private:
+  void debugOutVector(vector<int> inVector);
+
+};
+
+#endif /* CINTCODECOMPUTER_H_ */
