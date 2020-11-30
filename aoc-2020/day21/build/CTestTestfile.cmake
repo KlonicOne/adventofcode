2adcba16cb05e2deac110a163788d957ef6d3896
KlonicOne <klonicone@gmx.net>
2020-11-30 18:21:20 +0100

    Prepared cmake structure for aoc 20

---
+++ aoc-2020/day21/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: c:/msys64/home/Nico/repos/aoc/aoc-2020/day01
+# Build directory: c:/msys64/home/Nico/repos/aoc/aoc-2020/day01/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
