23958c9097f4206c5542ed9eb0802c3346146805
KlonicOne <KlonicOne@gmx.de>
2020-12-12 08:26:46 +0100

    Prepared day12

---
+++ aoc-2020/day12/day12.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file day12.cpp
+ * @author klonicone
+ * @version 0.1
+ * @date 2020-12-01
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+#include "day12.h"
+#include "show_container.h"
+
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <istream>
+#include <map>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+#define DEBUG_OUT false
+
+/** 
+ * @brief constructor
+ *
+ */
+day12::day12(/* args */) { }
+
+/**
+ * @brief Destroy the day12::day12
+ *
+ */
+day12::~day12() {}
+
+/**
+ * @brief Solve part 1
+ *
+ */
+void day12::solver_part1(void) {
+  int answer = 0;
+
+  // My bag in list so result - 1 should be good
+  std::cout << "Result Part1: " << answer << std::endl;
+}
+
+/**
+ * @brief Solve part 2
+ *
+ */
+void day12::solver_part2(void) {
+  int answer = 0;
+
+  // My bag in list so result - 1 should be good
+  std::cout << "Result Part2: " << answer << std::endl;
+  }
+
+/************************************************************/
+/************************************************************/
+/************************************************************/
---
+++ aoc-2020/day12/day12.h
@@ -0,0 +1,42 @@
+/**
+ * @file day12.h
+ * @author klonicone
+ * @version 0.1
+ * @date 2020-12-01
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
+#ifndef _DAY09_H_
+#define _DAY09_H_
+
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <istream>
+#include <map>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+class day12 {
+private:
+  /* data */
+
+public:
+  day12(/* args */);
+  ~day12();
+
+  // Part1
+  void solver_part1(void);
+
+  // Part2
+  void solver_part2(void);
+
+  // Common
+
+};
+
+#endif // _DAY06_H_
