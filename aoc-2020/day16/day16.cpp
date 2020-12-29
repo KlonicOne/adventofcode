fbdcdf89d079453f0e41351ce8ee68c343c6f383
KlonicOne <KlonicOne@gmx.de>
2020-12-29 18:11:13 +0100

    Rules are read correct

---
+++ aoc-2020/day16/day16.cpp
@@ -0,0 +1,155 @@
+/**
+ * @file day16.cpp
+ * @author klonicone
+ * @version 0.1
+ * @date 2020-12-01
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+#include "day16.h"
+#include "show_container.h"
+
+#include <algorithm>
+#include <chrono>
+#include <cstring>
+#include <iostream>
+#include <istream>
+#include <map>
+#include <numeric>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+#define DEBUG_OUT true
+
+/**
+ * @brief constructor
+ *
+ */
+day16::day16(/* args */) {}
+
+/**
+ * @brief Destroy the day16::day16
+ *
+ */
+day16::~day16() {}
+
+/**
+ * @brief Solve part 1
+ *
+ */
+void day16::solver_part1(void) {
+  unsigned long long answer = 0;
+
+  // Out result
+  std::cout << "Result Part1: " << std::dec << answer << std::endl;
+}
+
+/**
+ * @brief Solve part 2
+ *
+ */
+void day16::solver_part2(void) {
+  long answer = 0;
+
+  // Out result
+  std::cout << "Result Part2: " << answer << std::endl;
+}
+
+/************************************************************/
+/************************************************************/
+/************************************************************/
+
+/**
+ * @brief Split the intput string and store in class code list
+ *
+ * @param inTable
+ */
+void day16::format_input(std::vector<std::string> inTable) {
+  t_input_section current_section = RULES;
+  int line_num = 0;
+  std::string string_line = "";
+
+  for (std::vector<std::string>::const_iterator i = inTable.begin();
+       i != inTable.end(); ++i, ++line_num) {
+    std::size_t found;
+    std::size_t found_enclosing;
+    t_rule temp_rule;
+    t_range temp_range;
+
+    // Get single line as string_line
+    string_line = (*i);
+
+    // Skip empty line
+    if (string_line == "") {
+      // We completed section so go over to next
+      switch (current_section) {
+      case RULES: {
+        current_section = MY_TICKET;
+      } break;
+
+      case MY_TICKET: {
+        current_section = NEARBY_TICKET;
+      } break;
+
+      default:
+        break;
+      }
+      // Skip the empty line
+      continue;
+    }
+
+    // Which section of input
+    switch (current_section) {
+    case RULES: {
+      // We are reading rules, start with name up to : and then ranges are
+      // coming
+      found = string_line.find(":");
+      // Extract the rule name
+      std::string rule_name = string_line.substr(0, 0 + found);
+      temp_rule.name = rule_name;
+      // Rule has always two ranges
+      found_enclosing = string_line.find("or");
+      std::string rule1 =
+          string_line.substr(found + 2, found_enclosing - found - 3);
+      std::string rule2 = string_line.substr(
+          found_enclosing + 3, std::string::npos - found_enclosing);
+      // Extract the numbers of the rules and set the to temp rule
+      found = rule1.find("-");
+      temp_range.min_val = stoi(rule1.substr(0, found));
+      temp_range.max_val =
+          stoi(rule1.substr(found + 1, std::string::npos - found + 1));
+      temp_rule.ranges.push_back(temp_range);
+      found = rule2.find("-");
+      temp_range.min_val = stoi(rule2.substr(0, found));
+      temp_range.max_val =
+          stoi(rule2.substr(found + 1, std::string::npos - found + 1));
+      temp_rule.ranges.push_back(temp_range);
+      // Add temp rule to input data of class
+      this->m_rules.push_back(temp_rule);
+    } break;
+
+    case MY_TICKET: {
+    } break;
+
+    case NEARBY_TICKET: {
+    } break;
+
+    default:
+      break;
+    }
+  }
+
+  if (DEBUG_OUT) {
+    for (auto iter : this->m_rules) {
+      std::cout << iter.name << ": ";
+      for (auto iter_ranges : iter.ranges) {
+        std::cout << iter_ranges.min_val << "-" << iter_ranges.max_val
+                  << " or ";
+      }
+      std::cout << std::endl;
+    }
+  }
+}
---
+++ aoc-2020/day16/day16.h
@@ -0,0 +1,81 @@
+/**
+ * @file day16.h
+ * @author klonicone
+ * @version 0.1
+ * @date 2020-12-01
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
+#ifndef _DAY16_H_
+#define _DAY16_H_
+
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <istream>
+#include <map>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+typedef enum t_input_section {
+  RULES = 0,
+  MY_TICKET,
+  NEARBY_TICKET
+} t_input_section;
+
+/**
+ * @brief defines a range of values
+ *
+ */
+typedef struct t_range {
+  int min_val;
+  int max_val;
+} t_range;
+
+/**
+ * @brief single ruly type
+ *
+ */
+typedef struct t_rule {
+  std::string name;
+  std::vector<t_range> ranges;
+} t_rule;
+
+/**
+ * @brief Type for a ticket
+ *
+ */
+typedef struct t_ticket {
+  std::vector<int> ticket_vals;
+} t_ticket;
+
+/**
+ * @brief Class to realize the tasks for the day
+ *
+ */
+class day16 {
+private:
+  /* data */
+  std::vector<t_rule> m_rules;
+  t_ticket m_my_ticket;
+  std::vector<t_ticket> m_nearby_tickets;
+
+public:
+  day16(/* args */);
+  ~day16();
+
+  // Part1
+  void solver_part1(void);
+
+  // Part2
+  void solver_part2(void);
+
+  // Common
+  void format_input(std::vector<std::string> inTable);
+};
+
+#endif
